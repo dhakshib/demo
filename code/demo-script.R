98f0b2b4ddad0405ecdcf057565e6eb45e7fa5b7
Dhakshi Balakumar <dhakshi.balakumar@att.net>
2019-10-03 15:28:37 -0700

    new script file

---
+++ code/demo-script.R
@@ -0,0 +1,6 @@
+# Title: Short title (one sentence)
+# Description: what the script is about (one paragraph or two)
+# Input(s): what are the main inputs (list of inputs)
+# Output(s): what are the main outputs (list of outputs)
+# Author(s): First Last
+# Date: mm-dd-yyyy
