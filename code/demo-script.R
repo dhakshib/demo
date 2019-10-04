f7e468a3cf11e6dfcf9f2c33101d1ae5515e7c47
Dhakshi Balakumar <dhakshi.balakumar@att.net>
2019-10-04 14:58:10 -0700

    data-structure txt in outputs

--- code/demo-script.R
+++ code/demo-script.R
@@ -13,4 +13,23 @@
 library(dplyr)    # data wrangling
 library(ggplot2)  # graphics
 
-read.csv("data/nba2018-players.csv")
+# Use read.csv() to import the data nba2018-players.csv in R. Do 
+# this by specifying a relative path.
+dat <- read.csv("data/nba2018-players.csv")
+
+# Use the imported table to create a data frame warriors by selecting 
+# rows---e.g. filter()---of Golden State Warriors, arranging rows by 
+# salary in increasing order.
+warriors <- arrange(dat, salary)
+
+# Use the function write.csv() to export (or save) the data frame 
+# warriors to a data file warriors.csv in the data/ directory. You 
+# will need to use a relative path to specify the file argument. 
+# Also, see how to use the argument row.names to avoid including a 
+# first column of numbers.
+write.csv(warriors, file = "warriors.csv", row.names = FALSE)
+
+# Export the output of str() on the data frame with all the players.
+sink(file = "../outputs/data-structure.txt")
+str(dat$player)
+sink()
